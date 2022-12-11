cd88e26e1335c6201273cc515c9d3f1bd8d664e4
LBF38 <contact.lbf38@gmail.com>
2022-12-11 16:08:54 +0100

    Type Assertions

--- Module 02 Exercise/module02.ts
+++ Module 02 Exercise/module02.ts
@@ -21,6 +21,18 @@ let randomValue2: unknown = 10;
 randomValue2 = true;
 randomValue2 = 'Mateo';
 
-console.log(randomValue2.name);  // Error: Object is of type unknown
-randomValue2();                  // Error: Object is of type unknown
-randomValue2.toUpperCase();      // Error: Object is of type unknown
+// console.log(randomValue2.name);  // Error: Object is of type unknown
+// randomValue2();                  // Error: Object is of type unknown
+// randomValue2.toUpperCase();      // Error: Object is of type unknown
+
+// Type Assertions
+let randomValue3: unknown = 10;
+
+randomValue3 = true;
+randomValue3 = 'Mateo';
+
+if (typeof randomValue3 === "string") {
+    console.log((randomValue3 as string).toUpperCase());    //* Returns MATEO to the console.
+} else {
+    console.log("Error - A string was expected here.");    //* Returns an error message.
+}
