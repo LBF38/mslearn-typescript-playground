2078425777caf993f04ac65098acbfbc509860f2
LBF38 <contact.lbf38@gmail.com>
2022-12-11 16:05:27 +0100

    Playing with Unknown types

--- Module 02 Exercise/module02.ts
+++ Module 02 Exercise/module02.ts
@@ -15,3 +15,12 @@ randomValue = true;      // OK
 // console.log(randomValue.name);  // Logs "undefined" to the console
 // randomValue();                  // Returns "randomValue is not a function" error
 // randomValue.toUpperCase();      // Returns "randomValue is not a function" error
+
+// Unknown Type
+let randomValue2: unknown = 10;
+randomValue2 = true;
+randomValue2 = 'Mateo';
+
+console.log(randomValue2.name);  // Error: Object is of type unknown
+randomValue2();                  // Error: Object is of type unknown
+randomValue2.toUpperCase();      // Error: Object is of type unknown
