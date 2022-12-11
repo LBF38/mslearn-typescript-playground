49a57c55ac68c6e6ac248b5618dfe088f7c89f9d
LBF38 <contact.lbf38@gmail.com>
2022-12-11 16:02:25 +0100

    Playing with Any Type

--- Module 02 Exercise/module02.ts
+++ Module 02 Exercise/module02.ts
@@ -1,3 +1,4 @@
+// Enum Type
 enum ContractStatus {
     Permanent = 1,
     Temp,
@@ -6,3 +7,11 @@ enum ContractStatus {
 let employeeStatus: ContractStatus = ContractStatus.Temp;
 console.log(employeeStatus);
 console.log(ContractStatus[employeeStatus]);
+
+// Any Type
+let randomValue: any = 10;
+randomValue = 'Mateo';   // OK
+randomValue = true;      // OK
+// console.log(randomValue.name);  // Logs "undefined" to the console
+// randomValue();                  // Returns "randomValue is not a function" error
+// randomValue.toUpperCase();      // Returns "randomValue is not a function" error
