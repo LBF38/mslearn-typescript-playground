0a5162c25681eaa0708954689e5f2d2e73d8a351
LBF38 <contact.lbf38@gmail.com>
2022-12-11 15:58:25 +0100

    Playing with enum

--- Module 02 Exercise/module02.ts
+++ Module 02 Exercise/module02.ts
@@ -1,7 +1,8 @@
 enum ContractStatus {
-    Permanent,
+    Permanent = 1,
     Temp,
     Apprentice
 }
 let employeeStatus: ContractStatus = ContractStatus.Temp;
 console.log(employeeStatus);
+console.log(ContractStatus[employeeStatus]);
