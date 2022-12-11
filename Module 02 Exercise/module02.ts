67bd876c03c4954d8b747d7381a2d42df561ec91
LBF38 <contact.lbf38@gmail.com>
2022-12-11 16:19:25 +0100

    Playing with Union Type

--- Module 02 Exercise/module02.ts
+++ Module 02 Exercise/module02.ts
@@ -36,3 +36,22 @@ if (typeof randomValue3 === "string") {
 } else {
     console.log("Error - A string was expected here.");    //* Returns an error message.
 }
+
+// Union Type
+let multiType: number | boolean;
+multiType = 20;         //* Valid
+multiType = true;       //* Valid
+// multiType = "twenty";   //* Invalid
+
+function add(x: number | string, y: number | string) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x + y;
+    }
+    if (typeof x === 'string' && typeof y === 'string') {
+        return x.concat(y);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+console.log(add('one', 'two'));  //* Returns "onetwo"
+console.log(add(1, 2));          //* Returns 3
+console.log(add('one', 2));      //* Returns error
