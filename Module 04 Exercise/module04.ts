64d9e09e9cb27fa6c70d92076651c2b7fc0f6280
LBF38 <contact.lbf38@gmail.com>
2023-02-11 16:06:36 +0100

    update: module 04 playground

--- Module 04 Exercise/module04.ts
+++ Module 04 Exercise/module04.ts
@@ -19,3 +19,32 @@ function sum(input: number[]): number {
 
 sum([1, 2, 3, 4, 5]);
 // sum([1, 2, 3, 4, 5, "6"]); // Error : Type 'string' not assignable to type 'number'
+
+let addAllNumbers = (
+  firstNumber: number,
+  ...restOfNumbers: number[]
+): number => {
+  let total: number = firstNumber;
+  for (let counter = 0; counter < restOfNumbers.length; counter++) {
+    if (isNaN(restOfNumbers[counter])) {
+      continue;
+    }
+    total += Number(restOfNumbers[counter]);
+  }
+  return total;
+};
+
+addAllNumbers(1, 2, 3, 4, 5, 6, 7); // returns 28
+addAllNumbers(2); // returns 2
+// addAllNumbers(2, 3, "three");        // flags error due to data type at design time, returns 5
+
+interface Message {
+  text: string;
+  sender: string;
+}
+
+function displayMessage({ text, sender }: Message) {
+  console.log(`Message from ${sender}: ${text}`);
+}
+
+displayMessage({ sender: "Christopher", text: "hello, world" });
