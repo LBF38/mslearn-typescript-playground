414bb7fe0feae70bbd08b87f3f9f46815e9dedbc
LBF38 <contact.lbf38@gmail.com>
2023-02-11 19:06:47 +0100

    update: extending the Car class

--- Module 05 Playground/module05.ts
+++ Module 05 Playground/module05.ts
@@ -64,7 +64,7 @@ class Car {
     return `${this.worker()} is turning ${direction}`;
   }
   // This function performs work for the other method functions
-  private worker(): string {
+  protected worker(): string {
     return this._make;
   }
 }
@@ -84,3 +84,37 @@ console.log(myCar1.brake());
 console.log(myCar1.turn("right"));
 
 console.log(Car.getNumberOfCars());
+
+class ElectricCar extends Car {
+  // Properties unique to ElectricCar
+  private _range: number;
+
+  // Constructor
+  constructor(make: string, color: string, range: number, doors = 2) {
+    super(make, color, doors);
+    this._range = range;
+  }
+
+  // Accessors
+  get range() {
+    return this._range;
+  }
+  set range(range) {
+    this._range = range;
+  }
+
+  // Methods
+  charge() {
+    console.log(this.worker() + " is charging.");
+  }
+  // Overrides the brake method of the Car class
+  brake(): string {
+    return `${this.worker()}  is braking with the regenerative braking system.`;
+  }
+}
+
+let spark = new ElectricCar("Spark Motors", "silver", 124, 2);
+let eCar = new ElectricCar("Electric Car Co.", "black", 263);
+console.log(eCar.doors); // returns the default, 2
+spark.charge(); // returns "Spark Motors is charging"
+console.log(spark.brake()); // returns "Spark Motors is braking with the regenerative braking system"
